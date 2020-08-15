53dfedc95b181026b792b5c38dc418aa33f345d7
tianqingzx <2279267143@qq.com>
2020-08-15 16:59:10 +0800

    update 2020/8/15

---
+++ notes/Python编程技术/algorithm/data_structure/linear_list/link_list.py
@@ -0,0 +1,80 @@
+"""
+"""
+
+
+# 此处为单链表的结点
+class SLNode(object):
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+# 此处为双链表的结点
+class DLNode(object):
+    def __init__(self, data):
+        self.data = data
+        self.prior = None
+        self.next = None
+
+
+class SingleLinkList(object):
+    def __init__(self):
+        self.head = SLNode(None)
+
+    def list_insert(self, i, e):
+        """
+        algorithm: 在单链表中找到第 i-1 个结点，然后由 p 指向它。
+                若存在这样的结点，将值为 e 的结点插入到 p 所指结点之后。
+        :param i:
+        :param e:
+        :return:
+        """
+        j = 0
+        p = self.head
+        if i <= 0:
+            return False
+        while j < i-1 and p is not None:
+            j += 1
+            p = p.next
+        if p is None:
+            return False
+        else:
+            s = SLNode(e)
+            s.next = p.next
+            p.next = s
+            return True
+
+    def list_delete(self, i):
+        pass
+
+    def get_elem(self, i):
+        pass
+
+    def locate_elem(self, e):
+        pass
+
+    def disp_list(self):
+        p = self.head.next
+        while p is not None:
+            print(" {0} ".format(p.data), end="")
+            p = p.next
+        print("\n")
+
+
+class DoubleLinkList(object):
+    def __init__(self):
+        self.head = DLNode(None)
+
+    def list_insert(self, i, e):
+        pass
+
+
+def main():
+    link_list = SingleLinkList()
+    for i in range(5):
+        link_list.list_insert(1, i)
+    link_list.disp_list()
+
+
+if __name__ == '__main__':
+    main()
---
+++ notes/Python编程技术/algorithm/data_structure/linear_list/sequence_list.py
@@ -0,0 +1,67 @@
+"""
+
+"""
+
+
+class SqList(object):
+    def __init__(self, max_size):
+        """
+        初始化类实例
+        :param max_size: 限制最大数组长度，模拟C语言数组
+        """
+        self.data = list()
+        self.length = 0
+        self.max_size = max_size
+
+    def list_insert(self, i, e):
+        if i < 1 or i > (self.length + 1):
+            return False
+        if self.length >= self.max_size:
+            return False
+        for j in range(self.length, i-1, -1):
+            self.data[j] = self.data[j-1]
+        self.data[i-1] = e
+        self.length += 1
+        return True
+
+    def list_delete(self, i):
+        """
+
+        :param i:
+        :return: True | False, e | None
+        """
+        if i < 1 or i > self.length:
+            return False, None
+        e = self.data[i-1]
+        for j in range(i, self.length):  # 将i之后的元素往前移动一位
+            self.data[i-1] = self.data[i]
+        self.length -= 1
+        return True, e
+
+    def locate_elem(self, e):
+        """
+        按元素值 e 进行查找，返回元素位序
+        :param e:
+        :return: (i + 1) | 0 元素位序
+        """
+        for i in range(self.length):
+            if self.data[i] == e:
+                return i + 1
+        return 0
+
+    def get_elem(self, i):
+        """
+        按元素位序 i 进行查找，返回元素值
+        :param i:
+        :return: True | False, e | None
+        """
+        if i < 1 or i > self.length:
+            return False, None
+        return True, self.data[i-1]
+
+    def disp_list(self):
+        """
+        输出顺序表中元素的值
+        :return:
+        """
+        pass
